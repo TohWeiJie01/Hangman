99d63f65ae5a2288dd93f4cd865943637e8bb09f
TohWeiJie01 <tohweijie01@gmail.com>
2022-05-25 15:01:34 +0800

    Add Stickman figure for project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import Stickman from './components/Stickman';
 
 function App() {
   return (
     <div>
       <Header />
+      <div className='game-container'>
+        <Stickman />
+      </div>
     </div>
   )
 }
