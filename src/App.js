4dc194bee0ab6dd7395207753724cf6f88f272e3
TohWeiJie01 <tohweijie01@gmail.com>
2022-05-25 15:08:48 +0800

    Add Word to project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Stickman from './components/Stickman';
 import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className='game-container'>
         <Stickman />
         <WrongLetters />
+        <Word />
       </div>
     </div>
   )
---
+++ src/components/Word.js
@@ -0,0 +1,7 @@
+const Word = () => {
+    return (
+        <div class="word" id="word"></div>
+    )
+}
+
+export default Word
