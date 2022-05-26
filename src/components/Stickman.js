133f8b748e4774cbd78b74c6fb2fff436d2fa77b
TohWeiJie01 <tohweijie01@gmail.com>
2022-05-27 00:59:40 +0800

    Add display of stickman to project

--- src/components/Stickman.js
+++ src/components/Stickman.js
@@ -1,27 +1,32 @@
 import React from 'react';
 
-const Stickman = () => {
+const Stickman = ({wrongLetters}) => {
+  const numOfWrongLetters = wrongLetters.length;
+
   return (
-    <svg height="250" width="200" class="stickman-container">
-      {/*Rod*/}
-      <line x1="60" y1="20" x2="140" y2="20" />
-      <line x1="140" y1="20" x2="140" y2="50" />
-      <line x1="60" y1="20" x2="60" y2="230" />
-      <line x1="20" y1="230" x2="100" y2="230" />
+    <svg height="250" width="200" className="stickman-container">
+      {/*Pole*/}
+      {numOfWrongLetters > 0 && 
+      <>
+        <line x1="60" y1="20" x2="140" y2="20" /> 
+        <line x1="140" y1="20" x2="140" y2="50" /> 
+        <line x1="60" y1="20" x2="60" y2="230" /> 
+        <line x1="20" y1="230" x2="100" y2="230" />
+      </>}
 
       {/*Head*/}
-      <circle cx="140" cy="70" r="20" class="stickman-part" />
+      {numOfWrongLetters > 1 && <circle cx="140" cy="70" r="20" />}
     
       {/*Body*/}
-      <line x1="140" y1="90" x2="140" y2="150" class="stickman-part" />
+      {numOfWrongLetters > 2 && <line x1="140" y1="90" x2="140" y2="150" />}
     
       {/*Arms*/}
-      <line x1="140" y1="120" x2="120" y2="100" class="stickman-part" />
-      <line x1="140" y1="120" x2="160" y2="100" class="stickman-part" />
+      {numOfWrongLetters > 3 && <line x1="140" y1="120" x2="120" y2="100" />}
+      {numOfWrongLetters > 4 && <line x1="140" y1="120" x2="160" y2="100" />}
     
       {/*Legs*/}
-      <line x1="140" y1="150" x2="120" y2="180" class="stickman-part" />
-      <line x1="140" y1="150" x2="160" y2="180" class="stickman-part" />
+      {numOfWrongLetters > 5 && <line x1="140" y1="150" x2="120" y2="180" />}
+      {numOfWrongLetters > 6 && <line x1="140" y1="150" x2="160" y2="180" />}
     </svg>
   );
 }
