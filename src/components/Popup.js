cc711feafdef71122219032e7480f30338f75a5e
TohWeiJie01 <tohweijie01@gmail.com>
2022-05-29 16:07:23 +0800

    Add Popup functionality

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-const Popup = () => {
-    return (
-      <div class="popup-container" id="popup-container">
-        <div class="popup">
-          <h2 id="final-message"></h2>
-          <h3 id="final-message-reveal-word"></h3>
-          <button id="play-button">Play Again</button>
-        </div>
-      </div>
-    );
+const Popup = ({setCanPlay, correctLetters, mistakes, currentWord, playAgain}) => {
+  let canPlay = true;
+  let outcomeMessage = "";
+  let answerMessage = "";
+  let outcome = "pass";
+  let currentLetters = currentWord.split("");
+
+  const pass = currentLetters.forEach(letter => {
+    if (!(correctLetters.includes(letter))) {
+      outcome = " ";
+    }
+  });
+  const fail = mistakes >= 7;
+
+  if (outcome === "pass") {
+    outcomeMessage = ("YOU ARE GOOD AT HANGMAN!");
+    canPlay = false;
   }
+
+  if (fail) {
+    outcomeMessage = ("TRY AGAIN NEXT TIME!");
+    answerMessage = `The word is: ${currentWord}`;
+    canPlay = false;
+  }
+
+  useEffect(() => setCanPlay(canPlay));
+
+  return (
+    <div className="popup-container" style={outcomeMessage !== '' ? {display:'flex'} : {}}>
+      <div className="popup">
+        <h2>{outcomeMessage}</h2>
+        <h3>{answerMessage}</h3>
+        <button onClick={playAgain}>Play Again</button>
+      </div>
+    </div>
+  );
+}
   
-  export default Popup;
+export default Popup;
   
